70b63b92faa5bde6541f9d02993b782f4f0267b0
zach weinfurt <weinfz@gmail.com>
2018-09-10 11:33:06 -0400

    scrapper working with cookies pop up

--- lobus_scraper/lobus_scraper.py
+++ lobus_scraper/lobus_scraper.py
@@ -3,16 +3,19 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, NoSuchElementException, ElementNotVisibleException
+from selenium.common.exceptions import WebDriverException, TimeoutException, NoSuchElementException, ElementNotVisibleException
 import urllib
 import urlparse
 import pandas as pd
+import os
 
 class LobusScraper(object):
     def __init__(self):
         chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        self.driver = webdriver.Chrome(executable_path='../chromedriver', options=chrome_options)
+        #chrome_options.add_argument("--headless")
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        exe_path = os.path.join(file_path,'..','chromedriver')
+        self.driver = webdriver.Chrome(executable_path=exe_path, options=chrome_options)
         
     def close_signup(self):
         """ method to close the signup the pops up
@@ -20,6 +23,14 @@ class LobusScraper(object):
         """
         close = self.driver.find_element_by_id('close_signup')
         close.click()
+        
+    def accept_cookies(self):
+        """ method to accept cookies
+        """
+        self.driver.implicitly_wait(10)
+        accept = WebDriverWait(self.driver, 10).until(
+                    EC.invisibility_of_element_located((By.CLASS_NAME, 'optanon-allow-all')))
+        self.driver.execute_script("(arguments[0]).click();", accept)
 
     def get_lot_urls(self, url):
         """ method to retireve lot urls from an auction url
@@ -45,6 +56,11 @@ class LobusScraper(object):
             # must be no load all button they may all be showing
             # just continue
             pass
+        except WebDriverException:
+            ## accept cookies came up
+            self.accept_cookies()
+            urls = self.get_lot_urls(url)
+            return urls
         lot_elems = self.driver.find_elements_by_xpath('//*[@id="ResultContainer"]//a[@href]')
         urls = []
         for elem in lot_elems:
@@ -82,7 +98,9 @@ class LobusScraper(object):
             src = image.get_attribute('src')  
             ## save file to local if prod should should cloud storage
             filename = urlparse.urlparse(src).path.split('/')[-1]
-            urllib.urlretrieve(src, '../images/'+filename)
+            file_path = os.path.dirname(os.path.abspath(__file__))
+            save_path = os.path.join(file_path,'..','images',filename)
+            urllib.urlretrieve(src, save_path)
             # image meta data 
             row['image_url'] = src
             row['image_filename'] = filename
@@ -108,8 +126,32 @@ class LobusScraper(object):
                 row = self.parse_lot_url(url)
             data.append(row)
         return data
-    
+
     def scrape_auction_url(self, url):
         urls = self.get_lot_urls(url)
         data = self.parse_lot_urls(urls)
         return data
+    
+def save_raw_auction_data(url):
+    scraper = LobusScraper()
+    data = scraper.scrape_auction_url(url)
+    df = pd.DataFrame(data)
+    filename = urlparse.urlparse(url).path.split('/')[-1].split('.')[0]
+    file_path = os.path.dirname(os.path.abspath(__file__))
+    save_path = os.path.join(file_path,'..','data',filename+'.csv')
+    df.to_csv(save_path, encoding='utf-8')
+    
+    
+    
+    
+if __name__ == "__main__":
+    urls = [
+            "https://www.christies.com/impressionist-and-modern-art-27255.aspx?lid=1&dt=100420180204&saletitle=",
+            "https://www.christies.com/impressionist-and-modern-art-27400.aspx?lid=1&dt=100420180206&saletitle=",
+            ]
+    for url in urls:
+        save_raw_auction_data(url)
+    
+    
+    
+    
